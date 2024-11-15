be10d1917999dafe3c8fe1e66fea771f527c5bbe
chloetohce <chloetohce@gmail.com>
2024-11-15 13:41:26 +0800

    Initial commit

---
+++ src/main/java/sg/edu/nus/iss/ssf_11w/Ssf11wApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_11w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssf11wApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssf11wApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssf_11w/Ssf11wApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_11w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssf11wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
