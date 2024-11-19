5a89c2d16c3dff839ce6752574c7a27f3e398451
chloetohce <chloetohce@gmail.com>
2024-11-19 12:36:18 +0800

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# sdf_11w
+# SSF Day 11 Workshop Notes
+
+## Changing port
+`mvn spring-boot:run -Dserver.port=3000` or `java -Dserver.port=3000 -jar app.jar`
+
+will alter the port used if the port is **not set** by the app itself. 
+
+
+`mvn spring-boot:run -Dspring-boot.run.argument="--port=3000"`
+
+is how you can pass custom CLI arguments to the app. You will need to manually read them. 
--- src/main/java/sg/edu/nus/iss/ssf_11w/Ssf11wApplication.java
+++ src/main/java/sg/edu/nus/iss/ssf_11w/Ssf11wApplication.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.ssf_11w;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ssf11wApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ssf11wApplication.class, args);
+		SpringApplication app = new SpringApplication(Ssf11wApplication.class);
+		String port = "3000";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port"))
+			port = cliOpts.getOptionValues("port").get(0);
+		
+		app.setDefaultProperties(
+			Collections.singletonMap("server.port", port)
+		);
+
+		app.run(args);
+
 	}
 
 }
